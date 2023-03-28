107c5724beb4f5f73b55336e2c8dc65c6af08f68
skocherry <svetlemk@gmail.com>
2023-03-28 13:23:39 -0500

    us 6 updated

--- src/test/java/com/library/steps/US06_StepDef.java
+++ src/test/java/com/library/steps/US06_StepDef.java
@@ -1,5 +1,65 @@
 package com.library.steps;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class US06_StepDef {
+    @Given("the {string} on the home page")
+    public void the_on_the_home_page(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("the user navigates to {string} page")
+    public void the_user_navigates_to_page(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the librarian click to add book")
+    public void the_librarian_click_to_add_book() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the librarian enter book name {string}")
+    public void the_librarian_enter_book_name(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the librarian enter ISBN {string}")
+    public void the_librarian_enter_isbn(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the librarian enter year {string}")
+    public void the_librarian_enter_year(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the librarian enter author {string}")
+    public void the_librarian_enter_author(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the librarian choose the book category {string}")
+    public void the_librarian_choose_the_book_category(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the librarian click to save changes")
+    public void the_librarian_click_to_save_changes() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("verify {string} message is displayed")
+    public void verify_message_is_displayed(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("verify {string} information must match with DB")
+    public void verify_information_must_match_with_db(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
 
 }

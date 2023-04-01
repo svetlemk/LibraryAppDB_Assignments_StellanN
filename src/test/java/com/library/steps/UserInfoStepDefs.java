8d83d629d728c87d2230be01eb9b555fc9977ce1
Asbreez <aiyaiai@hotmail.com>
2023-03-31 22:52:27 -0500

    Revert "Update"
    This reverts commit 9bfc06ce03200f7abc1fc49e6796f43c90579640.

--- src/test/java/com/library/steps/UserInfoStepDefs.java
+++ src/test/java/com/library/steps/UserInfoStepDefs.java
@@ -1,7 +1,6 @@
 package com.library.steps;
 
 import com.library.utility.DB_Util;
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -10,7 +9,6 @@ import org.junit.Assert;
 public class UserInfoStepDefs {
 
     String actualUserCount;
-
     @Given("Establish the database connection")
     public void establish_the_database_connection() {
 
@@ -21,36 +19,32 @@ public class UserInfoStepDefs {
         System.out.println("**********************************************");
 
     }
-
     @When("Execute query to get all IDs from users")
     public void execute_query_to_get_all_i_ds_from_users() {
-        String query = "select count(id) from users"; // 1855
+        String query="select count(id) from users"; // 1855
         DB_Util.runQuery(query);
 
         actualUserCount = DB_Util.getFirstRowFirstColumn();
         System.out.println("actualUserCount = " + actualUserCount);
 
     }
-
     @Then("verify all users has unique ID")
     public void verify_all_users_has_unique_id() {
 
-        String query = "select count(distinct id) from users";
+        String query="select count(distinct id) from users";
         DB_Util.runQuery(query);
         String expectedUserCount = DB_Util.getFirstRowFirstColumn();
         System.out.println("expectedUserCount = " + expectedUserCount);
 
         // MAKE ASSERTIONS
-        Assert.assertEquals(expectedUserCount, actualUserCount);
+        Assert.assertEquals(expectedUserCount,actualUserCount);
 
         //CLOSE CONN
         // DB_Util.destroy();
         System.out.println("**********************************************");
         System.out.println("*** DESTROY  WILL BE DONE WITH HOOK CLASS***");
         System.out.println("**********************************************");
+
+
     }
 }
-
-
-
-

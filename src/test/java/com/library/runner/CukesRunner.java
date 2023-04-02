cb4ee97bdbda51eb8d80904ea961c6be2e22a350
daurentk <114819349+daurentk@users.noreply.github.com>
2023-04-02 09:08:08 -0500

    US01 test execution is added_Dauren K

--- src/test/java/com/library/runner/CukesRunner.java
+++ src/test/java/com/library/runner/CukesRunner.java
@@ -15,7 +15,9 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features" ,
         glue = "com/library/steps",
         dryRun = false,
+        //tags = @us01"
         tags = "@us04"
+
 )
 public class CukesRunner {
 
